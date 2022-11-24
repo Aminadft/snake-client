f1f3dac5acfb57962ba4df6e19e5556cd1843105
Amina Malik <amina.malik@telus.com>
2022-11-23 20:59:22 -0500

    completed

--- play.js
+++ play.js
@@ -1,17 +1,29 @@
-const net = require("net");
-
-// establishes a connection with the game server
+//const net = require("net");
+const {connect} = require('./client');
+ //establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost", 
+   host: "165.227.47.243", 
     port: 50541 
   });
 
-  // interpret incoming data as text
+   interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
 };
+// setup interface to handle user input from stdin
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function () {
+
+};
 console.log("Connecting ...");
 connect();
