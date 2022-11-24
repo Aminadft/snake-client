1514d8efe145e3dfbabdc69e8e6d54f87c9f48ad
Amina Malik <amina.malik@telus.com>
2022-11-23 22:44:55 -0500

    edited

--- input.js
+++ input.js
@@ -1,9 +1,17 @@
 const { constants } = require("./constants");
+const {MOVE_UP_KEY,MOVE_LEFT_KEY,MOVE_DOWN_KEY,MOVE_RIGHT_KEY,TAG_LINE_KEY,TAG_LINETWO_KEY } = require('./constants');
 
 // Stores the active TCP connection object.
 let connection;
 
+const up = MOVE_UP_KEY;
+const left =MOVE_LEFT_KEY;
+const down =MOVE_DOWN_KEY;
+const right =MOVE_RIGHT_KEY;
+const tag =TAG_LINE_KEY;
+const tagtwo =TAG_LINETWO_KEY;
 const { connect } = require("./client");
+const { keyword } = require("color-convert");
 const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
@@ -13,15 +21,15 @@ const setupInput = (conn) => {
   stdin.on("data", handleUserInput);
   return stdin;
 };
-const handleUserInput = function(input) {
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-    if (input === MOVE_UP_KEY) connection.write("Move: up");
-    if (input === MOVE_DOWN_KEY) connection.write("Move: down");
-    if (input === MOVE_LEFT_KEY) connection.write("Move: left");
-    if (input === MOVE_RIGHT_KEY) connection.write("Move: right");
-    if (input === "hi") connection.write("Say: Hello");
-    if (input === "bye") connection.write("Say: Goodbye");
+    if (key === up) connection.write("Move: up");
+    if (key === down) connection.write("Move: down");
+    if (key === left) connection.write("Move: left");
+    if (key === right) connection.write("Move: right");
+    if (key === tag) connection.write("Say: Hello");
+    if (key === tagtwo) connection.write("Say: Goodnight");
   }
 };
 console.log("Connecting ...");
