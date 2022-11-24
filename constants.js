f2115fdde31f7026d24f419f9ee6121f4a3f33d4
Amina Malik <amina.malik@telus.com>
2022-11-23 20:59:55 -0500

    completed constant list

---
+++ constants.js
@@ -0,0 +1,19 @@
+const IP = "192.168.68.247"; 
+const PORT = 50541;
+const NAME = "AM";
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const TAG_LINE_KEY = 'r';
+
+module.exports = {
+  IP,
+  PORT,
+  NAME,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  TAG_LINE_KEY,
+};
