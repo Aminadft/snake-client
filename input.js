9d31ab73418bbf39c85a6b44164b16d39df086c0
Amina Malik <amina.malik@telus.com>
2022-11-23 20:59:04 -0500

    completed

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
+const { constants } = require("./constants");
+
+// Stores the active TCP connection object.
+let connection;
+
 const { connect } = require("./client");
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,8 +13,16 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
-const handleUserInput = function () {
-
+const handleUserInput = function(input) {
+  if (key === '\u0003') {
+    process.exit();
+    if (input === MOVE_UP_KEY) connection.write("Move: up");
+    if (input === MOVE_DOWN_KEY) connection.write("Move: down");
+    if (input === MOVE_LEFT_KEY) connection.write("Move: left");
+    if (input === MOVE_RIGHT_KEY) connection.write("Move: right");
+    if (input === "hi") connection.write("Say: Hello");
+    if (input === "bye") connection.write("Say: Goodbye");
+  }
 };
 console.log("Connecting ...");
-module.exports = {setupInput};
+module.exports = { setupInput };
